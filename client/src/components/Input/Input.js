a1d3c631936207e0c19146ff875b41949f06c2a3
StJohnErvin <johnervin.ceriola@outlook.com>
2020-12-19 04:18:27 +0800

    client folder

---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Input.css';
+
+const Input = ({message, setMessage, sendMessage}) => (
+  <form className="form">
+    <input
+      className="input"
+      type="text"
+      placeholder="Type a message here..."
+      value={message}
+      onChange={({ target: { value } }) => setMessage(value)}
+      onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+    />
+    <button className="sendButton" onClick={(event) => sendMessage(event)}>Send</button>
+  </form>
+)
+
+export default Input;
